a227513fd8853f903302e769ac4d2fd1ea917827
Psyphant <dothy@arcor.de>
2013-04-19 13:11:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duhajo::Application.config.secret_token = 'e673ce1d018fd046f143b179abdd5f4c4e29d8d203df3d457e4829aa186418d917d0b4c94115b8416b46eddb6d93c65350e085924302494273fdebab01723c5c'
