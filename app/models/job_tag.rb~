0dffeac5f63db0e965f1846b17b0b13309476956
Psyphant <dothy@arcor.de>
2013-06-02 19:32:57 +0200

    gitignore test

--- app/models/job_tag.rb~
+++
@@ -1,5 +0,0 @@
-class JobTag < ActiveRecord::Base
-  attr_accessible :job_id, :tag_id
-  belongs_to :job
-  belongs_to :tag
-end
--- app/models/job_worker.rb
+++
@@ -1,5 +0,0 @@
-class JobWorker < ActiveRecord::Base
-  attr_accessible :job_id, :worker_id, :iscreator
-  belongs_to :user
-  belongs_to :job
-end
