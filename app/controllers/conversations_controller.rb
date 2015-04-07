372427997390dfae0b05ec6945f80683bb6e9745
Psyphant <dothy@arcor.de>
2015-04-07 18:05:40 +0200

    add first version of the chat

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,26 @@
+class ConversationsController < ApplicationController
+  before_filter :authenticate_user!
+
+  layout false
+
+  def create
+    if Conversation.between(params[:sender_id],params[:recipient_id]).present?
+      @conversation = Conversation.between(params[:sender_id],params[:recipient_id]).first
+    else
+      @conversation = Conversation.create!(:sender_id => params[:sender_id], :recipient_id => params[:recipient_id])
+    end
+
+    render json: { conversation_id: @conversation.id }
+  end
+
+  def show
+    @conversation = Conversation.find(params[:id])
+    @reciever = interlocutor(@conversation)
+    @messages = @conversation.messages
+    @message = Message.new
+  end
+
+  def interlocutor(conversation)
+    current_user == conversation.recipient ? conversation.sender : conversation.recipient
+  end
+end
