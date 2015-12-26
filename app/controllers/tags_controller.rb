446b7ccd7d744cd9590d1edb62d0d7153ef41793
Psyphant <dothy@arcor.de>
2015-12-26 18:47:17 +0100

    add a simple tag view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,8 +7,15 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
 
-    @workers = User.tagged_with(@tag.name)
-    @jobs = Job.tagged_with(@tag.name)
+    @users = User.tagged_with(@tag.name)
+    @supplies = Job.tagged_with(@tag.name).where(:type => 1)
+    @demands = Job.tagged_with(@tag.name).where(:type => 2)
+
+    @rel_tags_users = @users.collect{|x|x.skills}.flatten
+    @rel_tags_supplies = @supplies.collect{|x|x.skills}.flatten
+    @rel_tags_demands = @demands.collect{|x|x.skills}.flatten
+    @related_tags = (@rel_tags_users + @rel_tags_supplies + @rel_tags_demands).uniq
+    @related_tags.delete(@tag)
 
     respond_to do |format|
       format.html # show.html.erb.erb
