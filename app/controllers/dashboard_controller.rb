6932e6ba8e3a9475f47b0aa94a85214d56a81ba2
Psyphant <dothy@arcor.de>
2016-01-02 12:48:17 +0100

    .

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @skills = @user.tag_counts_on(:skills)
-    @recommendations = Job.tagged_with(@skills, :any => true).by_join_date
+    @recommendations = Job.tagged_with(@skills, :any => true).joins(:user).select("jobs.*, users.email as user_email, users.name as user_name, users.id as user_id").by_join_date
 
     @my_activities = PublicActivity::Activity.order("created_at DESC").joins("LEFT JOIN jobs ON activities.trackable_id = jobs.id").where('jobs.user_id' => @current_user.id).all
 
@@ -14,7 +14,7 @@ class DashboardController < ApplicationController
       else
         @box = Geocoder::Calculations.bounding_box(@center_point, 10)
       end
-      @near_jobs = Job.within_bounding_box(@box)
+      @near_jobs = Job.within_bounding_box(@box).joins(:user).select("jobs.*, users.email as user_email, users.name as user_name, users.id as user_id")
     end
   end
 end
